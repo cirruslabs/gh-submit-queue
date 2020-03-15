18225558427fea0d84a766da5c405e99db196c14
fkorotkov <fedor.korotkov@gmail.com>
2020-03-15 10:00:37 -0400

    Log requests while testing API client

--- src/test/kotlin/org/cirruslabs/sq/github/impl/GitHubAPITest.kt
+++ src/test/kotlin/org/cirruslabs/sq/github/impl/GitHubAPITest.kt
@@ -1,9 +1,17 @@
 package org.cirruslabs.sq.github.impl
 
+import io.ktor.client.HttpClient
+import io.ktor.client.engine.cio.CIO
+import io.ktor.client.features.json.JsonFeature
+import io.ktor.client.features.logging.DEFAULT
+import io.ktor.client.features.logging.LogLevel
+import io.ktor.client.features.logging.Logger
+import io.ktor.client.features.logging.Logging
 import io.ktor.util.KtorExperimentalAPI
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.runBlocking
+import org.cirruslabs.sq.github.api.CheckSuiteConclusion
 import org.cirruslabs.sq.github.api.PullRequestState
 import org.junit.Test
 import kotlin.test.assertEquals
@@ -11,7 +19,30 @@ import kotlin.test.assertEquals
 @ExperimentalCoroutinesApi
 @KtorExperimentalAPI
 class GitHubAPITest {
-  private val api = GitHubAPIImpl(TestGitHubAccessTokenManager)
+  private val api = GitHubAPIImpl(
+    TestGitHubAccessTokenManager,
+    httpClient = HttpClient(CIO) {
+      install(JsonFeature)
+      install(Logging) {
+        logger = Logger.DEFAULT
+        level = LogLevel.ALL
+      } // for debugging
+    }
+  )
+
+  @Test
+  fun listCheckSuites() {
+    runBlocking {
+      val suites = api.listCheckSuites(
+        installationId = 0L,
+        owner = "flutter",
+        repo = "flutter",
+        ref = "1ca4815035e6aa14831d8148062c732be3a458c4"
+      )
+      val suite = suites.first { it.app.name == "Cirrus CI" }
+      assertEquals(CheckSuiteConclusion.success, suite.conclusion)
+    }
+  }
 
   @Test
   fun listPRs() {
